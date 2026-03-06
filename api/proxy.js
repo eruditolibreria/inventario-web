d09b68e856259e75f5760af89a9ebf1b8473212c
eruditolibreria <eruditos.libreria@gmail.com>
2026-03-06 19:37:51 -0400

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,12 @@
+// api/proxy.js
+export default async function handler(req, res) {
+  const params = new URLSearchParams(req.query).toString();
+  const url = `https://script.google.com/macros/s/AKfycbxAJ_xsJgTzhydxSRcBIv9gYh83fawf4w5dCkAt2m50rYxD6Xa4UvC_yAscCmBPgQJfQA/exec?${params}`;
+
+  const response = await fetch(url, { redirect: 'follow' });
+  const text = await response.text();
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', response.headers.get('content-type') || 'text/plain');
+  res.status(200).send(text);
+}
