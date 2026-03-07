d3328f155adc13b85559d7e48c79c4a11eb4e5d7
eruditolibreria <eruditos.libreria@gmail.com>
2026-03-07 19:58:59 -0400

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 // api/proxy.js
 export default async function handler(req, res) {
   const params = new URLSearchParams(req.query).toString();
-  const url = `https://script.google.com/macros/s/AKfycbwBzXpYT75DhpCh3V7oz7fad6C5i4V0fhq3ukSvXJNyHKwLnVX8x5cP7dDGd8KoZ0sLFQ/exec?${params}`;
+  const url = `https://script.google.com/macros/s/AKfycbwVb5fq3__6yjgk5i9tFWoe7FHsOuSHgGCYrU8SUk56jAdoXjigZf9Z8Kp28Z7iMhOG2A/exec?${params}`;
 
   const response = await fetch(url, { redirect: 'follow' });
   const text = await response.text();
