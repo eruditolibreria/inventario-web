97bea11a2c47e5b893a2dc26e0ff4aa8c7d0ba6e
eruditoliberia <eruditos.libreria@gmail.com>
2026-06-23 19:47:26 -0400

    Mod-EfectoFacebookyPanelLiberado

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nombre de caché con versión (cámbialo cada vez que actualices la app)
-const CACHE_NAME = 'eruditos-v22';
+const CACHE_NAME = 'eruditos-v23';
 
 // Archivos a cachear (offline)
 const urlsToCache = [
