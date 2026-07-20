804af0314540c667925a944b77230456a7e61139
eruditoliberia <eruditos.libreria@gmail.com>
2026-07-20 10:29:27 -0400

    tel-esc-suc-front1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nombre de caché con versión (cámbialo cada vez que actualices la app)
-const CACHE_NAME = 'eruditos-v32';
+const CACHE_NAME = 'eruditos-v33';
 
 // Archivos a cachear (offline)
 const urlsToCache = [
