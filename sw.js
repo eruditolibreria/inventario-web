fd7bce723da1e1a8ce55ec0cbda9231ce2cdd401
eruditoliberia <eruditos.libreria@gmail.com>
2026-07-21 13:22:42 -0400

    FixEscaner-CodBarras

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nombre de caché con versión (cámbialo cada vez que actualices la app)
-const CACHE_NAME = 'eruditos-v34';
+const CACHE_NAME = 'eruditos-v35';
 
 // Archivos a cachear (offline)
 const urlsToCache = [
