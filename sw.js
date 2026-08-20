c6613eddb60540a78e1c884cbba32c12df61ba92
eruditoliberia <eruditos.libreria@gmail.com>
2026-08-20 19:35:08 -0400

    Pago_Cambio_mixto

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nombre de caché con versión (cámbialo cada vez que actualices la app)
-const CACHE_NAME = 'eruditos-v49';
+const CACHE_NAME = 'eruditos-v50';
 
 // Archivos a cachear (offline)
 const urlsToCache = [
