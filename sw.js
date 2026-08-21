c80b5d19bba1e258eecdf796ee2556549cf41b30
eruditoliberia <eruditos.libreria@gmail.com>
2026-08-21 17:07:57 -0400

    FixImagenInventario

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nombre de caché con versión (cámbialo cada vez que actualices la app)
-const CACHE_NAME = 'eruditos-v51';
+const CACHE_NAME = 'eruditos-v53';
 
 // Archivos a cachear (offline)
 const urlsToCache = [
